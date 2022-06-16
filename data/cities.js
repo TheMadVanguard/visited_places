13950641afbd23ba2b1338397b94bfdba3fb3783
Jay <37044649+TheMadVanguard@users.noreply.github.com>
2022-06-16 08:40:08 +0100

    Updates
    Updating .gitignore to ignore .bak files from notepad++
    
    Updated cities.js to include the format

--- data/cities.js
+++ data/cities.js
@@ -1,5 +1,6 @@
 CITIES =
 [
+   //{name: '', latitude: , longitude: , radius: 3, fillKey: 'city', date: ' 2020'},
    {name: 'Venice', latitude: 45.4408, longitude: 12.3155, radius: 3, fillKey: 'city', date: '1963-10'},
    // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
    {name: 'Khao Phing Kan', latitude: 8.2745, longitude: 98.5012, radius: 3, fillKey: 'city', date: '1974-12'},
